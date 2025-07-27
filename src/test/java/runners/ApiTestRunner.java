0aeae268c25659d29d838c6cb0254da48f7a4ebf
sanjeevi <sanjeeviponnusamy051@gmail.com>
2025-07-27 16:43:20 +0530

    NewCode

--- src/test/java/runners/ApiTestRunner.java
+++ src/test/java/runners/ApiTestRunner.java
@@ -7,12 +7,10 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features",
-        glue = "stepdefinitions",
-        plugin = {"pretty", "html:target/cucumber-reports.html"},
-        monochrome = true
+    features = "src/test/resources/features",
+    glue = "stepDefs",
+    plugin = {"pretty", "html:target/cucumber.html"},
+    monochrome = true
 )
 public class ApiTestRunner {
-            public static void main(String[] args) { ... }
-
 }
